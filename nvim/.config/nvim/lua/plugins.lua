34814d9f3552b7e6b0d51d4b6609b70b82610011
Ashley Towner <git@ashtowner.com>
2026-09-24 18:44:20 +0930

    chore(nvim): remove unused Telescope LSP handlers

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -23,7 +23,6 @@ vim.api.nvim_create_autocmd('PackChanged', {
 vim.pack.add({
 	'https://github.com/nvim-lua/plenary.nvim',
 	'https://github.com/nvim-telescope/telescope.nvim',
-	'https://github.com/gbrlsnchs/telescope-lsp-handlers.nvim',
 	'https://github.com/nvim-telescope/telescope-ui-select.nvim',
 	'https://github.com/tpope/vim-surround',
 	'https://github.com/tpope/vim-repeat',
